a531bb65c578e37d672ddd1494029cd16de20f78
“stanleycelestin1” <stanleycelestin1@gmail.comgit config --global user.namegit config --global user.email stanleycelestin1@gmail.com>
2017-10-13 12:30:57 -0400

    add variable to enable local run

--- config/env/development.js
+++ config/env/development.js
@@ -6,7 +6,7 @@ module.exports = {
   db: {
     //<dbuser>:<dbpassword>@ds161194.mlab.com:61194/loveddb
     //uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
-    uri:process.env.MONGODB_URI || process.env.PROD_MONGODB,
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://stanley:celestin@ds161194.mlab.com:61194/loveddb',
     options: {
       user: '',
       pass: ''
--- config/env/production.js
+++ config/env/production.js
@@ -9,7 +9,7 @@ module.exports = {
   port: process.env.PORT || 8443,
   db: {
     // env vars
-    uri: uri:process.env.MONGODB_URI || process.env.PROD_MONGODB,
+    uri: process.env.MONGODB_URI || process.env.PROD_MONGODB,
     options: {
       user: '',
       pass: ''
