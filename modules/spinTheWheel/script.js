4a99069499ac1e5ccefeb6be91fdc3c657795ad4
Prakash Pudhucode <optimusprike@users.noreply.github.com>
2017-10-27 17:34:58 -0400

    Add files via upload
    30 slices on wheel with updated client information

--- modules/spinTheWheel/script.js
+++ modules/spinTheWheel/script.js
@@ -4,18 +4,42 @@ var padding = {top:20, right:40, bottom:0, left:0},
             r = Math.min(w, h)/2,
             rotation = 0,
             oldrotation = 0,
-            picked = 100000,
-            oldpick = [],
+            pickedOption = 100000,
+            selectedOptions = [],
             color = d3.scale.ordinal()
-  .domain(["Option 1", "Option 2", "Option 3","Option 4", "Option 5", "Option 6"])
+  .domain(["Option 1", "Option 2", "Option 3","Option 4", "Option 5", "Option 6","Option 7", "Option 8", "Option 9","Option 10", "Option 11", "Option 12","Option 13", "Option 14", "Option 15","Option 16", "Option 17", "Option 18","Option 19", "Option 20", "Option 21","Option 22", "Option 23", "Option 24","Option 25", "Option 26", "Option 27","Option 28", "Option 29", "Option 30"])
   .range(["#fad654" , "#ffa309" , "#d97804", "#ed2937","#b4292e","#818185"]);
         var ways_to_grow_data = [
-                    {"label":"Option 1", "option":" Option 1"},
-                    {"label":"Option 2", "option":" Option 2"}, 
-                    {"label":"Option 3", "option":" Option 3"}, 
-                    {"label":"Option 4", "option":" Option 4"}, 
-                    {"label":"Option 5", "option":" Option 5"}, 
-                    {"label":"Option 6", "option":" Option 6"}, 
+                    {"label":"Option 1", "option":" Invite someone to go ahead of you in line."},
+                    {"label":"Option 2", "option":" Compliment 3 People."}, 
+                    {"label":"Option 3", "option":" Acknowledge someone's contribution."}, 
+                    {"label":"Option 4", "option":" Meditate for 30 minutes."}, 
+                    {"label":"Option 5", "option":" Hold the door open for people."}, 
+                    {"label":"Option 6", "option":" Create a bucket list and share it."}, 
+                    {"label":"Option 7", "option":" Create a gratitude list and share it."},
+                    {"label":"Option 8", "option":" Ask an elder about his/her childhood."}, 
+                    {"label":"Option 9", "option":" De-clutter your environment and donate."}, 
+                    {"label":"Option 10", "option":" Treate a stranger to a coffee."}, 
+                    {"label":"Option 11", "option":" Share some wisdom with your younger self."}, 
+                    {"label":"Option 12", "option":" Give someone a handmade card/gift."}, 
+                    {"label":"Option 13", "option":" Share your Strive for 5 progress with a friend."},
+                    {"label":"Option 14", "option":" Listen to personal growth podcast."}, 
+                    {"label":"Option 15", "option":" Stop using social media for today."}, 
+                    {"label":"Option 16", "option":" Share LovEd with someone who's struggling."}, 
+                    {"label":"Option 17", "option":" Watch a TED Talk on personal growth."}, 
+                    {"label":"Option 18", "option":" Have a date night with yourself."}, 
+                    {"label":"Option 19", "option":" Do something you enjoyed as a kid."},
+                    {"label":"Option 20", "option":" Write a letter to someone you admire."}, 
+                    {"label":"Option 21", "option":" Share something that people don't know about you."}, 
+                    {"label":"Option 22", "option":" List everyone you carry resentment for."}, 
+                    {"label":"Option 23", "option":" Write yourself a forgiveness letter."}, 
+                    {"label":"Option 24", "option":" Write an encouragement letter to your future self."}, 
+                    {"label":"Option 25", "option":" List what you've learned from past relationships."},
+                    {"label":"Option 26", "option":" Ask for feedback from someone."}, 
+                    {"label":"Option 27", "option":" Reach out to someone for advice."}, 
+                    {"label":"Option 28", "option":" Invite a negative person in your life to change."}, 
+                    {"label":"Option 29", "option":" Create a list of things you need to let go."}, 
+                    {"label":"Option 30", "option":" Apologize to someone."}, 
         ];
 
 
@@ -61,16 +85,14 @@ var padding = {top:20, right:40, bottom:0, left:0},
                 return ways_to_grow_data[i].label;
             });
 
-        container.on("click", spin);
+        container.on("click", spinWheel);
 
 
-        function spin(d){
+        function spinWheel(d){
             
             container.on("click", null);
 
-            //all slices have been seen, all done
-            console.log("OldPick: " + oldpick.length, "Data length: " + ways_to_grow_data.length);
-            if(oldpick.length == ways_to_grow_data.length){
+            if(selectedOptions.length == ways_to_grow_data.length){
                 d3.select("#option h1")
                         .text("All options have been chosen!");
             }
@@ -82,59 +104,49 @@ var padding = {top:20, right:40, bottom:0, left:0},
                 
             rotation = (Math.round(rng / ps) * ps);
             
-            picked = Math.round(ways_to_grow_data.length - (rotation % 360)/ps);
-            picked = picked >= ways_to_grow_data.length ? (picked % ways_to_grow_data.length) : picked;
+            pickedOption = Math.round(ways_to_grow_data.length - (rotation % 360)/ps);
+            pickedOption = pickedOption >= ways_to_grow_data.length ? (pickedOption % ways_to_grow_data.length) : pickedOption;
 
 
-            if(oldpick.indexOf(picked) !== -1){
-                d3.select(this).call(spin);
+            if(selectedOptions.indexOf(pickedOption) !== -1){
+                d3.select(this).call(spinWheel);
                 return;
             } else {
-                oldpick.push(picked);
+                selectedOptions.push(pickedOption);
             }
 
             rotation += 90 - Math.round(ps/2);
 
             vis.transition()
                 .duration(3000)
-                .attrTween("transform", rotTween)
+                .attrTween("transform", rotateWheel)
                 .each("end", function(){
-
-                    //mark question as seen
-                    //populate question
                     d3.select("#option h1")
-                        .text(ways_to_grow_data[picked].option);
-
+                        .text(ways_to_grow_data[pickedOption].option);
                     oldrotation = rotation;
-                
-                    container.on("click", spin);
+                    container.on("click", spinWheel);
                 });
         }
-
-        //make arrow
         svg.append("g")
             .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h/2)+padding.top) + ")")
             .append("path")
             .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
             .style({"fill":"black"});
 
-        //draw spin circle
         container.append("circle")
             .attr("cx", 0)
             .attr("cy", 0)
-            .attr("r", 60)
+            .attr("r", 50)
             .style({"fill":"white","cursor":"pointer"});
 
-        //spin text
         container.append("text")
             .attr("x", 0)
             .attr("y", 15)
             .attr("text-anchor", "middle")
-            .text("Spin")
+            .text("SPIN")
             .style({"font-weight":"bold", "font-size":"30px"});
         
-        
-        function rotTween(to) {
+        function rotateWheel(to) {
           var i = d3.interpolate(oldrotation % 360, rotation);
           return function(t) {
             return "rotate(" + i(t) + ")";
