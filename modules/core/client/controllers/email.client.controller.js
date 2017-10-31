3a6139166e3a6b1c89c996bf3ce592a2d01e035f
Jose Rivera <joserivera4497@gmail.com>
2017-10-30 23:58:54 -0400

    email verification added

--- modules/core/client/controllers/email.client.controller.js
+++ modules/core/client/controllers/email.client.controller.js
@@ -21,11 +21,16 @@ angular.module('core').controller('EmailController', ['$scope', 'Authentication'
 			'your response will be kept totally anonymous and they will only see an average of multiple scores. Don\'t keep them waiting! Click this link to rate your friend now.\"';
 			send.submit();
 		};
-		$scope.printEmail = function(){
-			$scope.emailstring = 'action=\"mailto:' + $scope.email1 + ',';
-			$scope.emailstring += $scope.email2 + ',';
-			$scope.emailstring += $scope.email3 + '\"';
-			console.log($scope.emailstring);
+		$scope.checkEmail = function(){
+			if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($scope.email3) && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($scope.email1) &&
+			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($scope.email2))
+  		{
+    		return false;
+  		}
+			else {
+				return true;
+			}
+
 		};
 
   }]);
