1dbcaf130ab2b9f9d2d713f875dfdd1721b28319
Stanley Celestin <stanleycelestin1@gmail.com>
2017-11-19 21:24:50 -0500

    Added Romantic Attraction Assessmentment and styled the radio answers

--- modules/assessments/client/controllers/emotional-maturity-assessment.client.controller.js
+++ modules/assessments/client/controllers/emotional-maturity-assessment.client.controller.js
@@ -22,6 +22,37 @@
       console.log("Submitted");
     }
 
+    vm.raa=["Physical affection with this person is very special.",
+            "I experience something special with this person that I do not experience with others.", 
+            "I am curious about why and how much this person is interested in me.", 
+            "I get excited when I meet this person unexpectedly.", 
+            "I am willing to change in order to be more desirable to this person.", 
+            "I have a longing for this person when we are apart.", 
+            "Being with this person is more important to me than where we are or what we are doing.", 
+            "I want to be very special in this person's life.", 
+            "I see this person as a long term companion.", 
+            "I am willing to continue this relationship in spite of any unpleasantness.", 
+            "I am willing to do things for this person without having to know the reason why.", 
+            "We often have a very good time even when we are not doing anything special.", 
+            "I get a thrill from just looking at this person.", 
+            "I enjoy this person in ways beyond just sharing affection.", 
+            "There is something almost mystical in our eye-to-eye contact.", 
+            "I could see myself growing old with this person.", 
+            "This person is a beautiful person (inner beauty).", 
+            "This person is often on my mind.", 
+            "I would feel sad if this person became strongly interested in someone else.", 
+            "I want our attraction to be mutual.", 
+            "I experience unusual and pleasantly exciting feelings when I am with this person.", 
+            "I want this person to respect me for my abilities.", 
+            "I enjoy being with this person even when we are silent.", 
+            "It means a lot to me when this person does something special for me.", 
+            "I would like to know what this person finds attractive about me.", 
+            "There are so many things I wish we could do together, if only there was enough time.", 
+            "I have a protective interest about this person's well being.", 
+            "I like doing things for this person.", 
+            "I have thoughts of what our future together might look like." 
+            ]
+
     vm.ema=["This person is aware of and able to express accurately how he/she feels.",
             "This person demonstrates sensitivity to how others are feeling.",
             "This person is willing and able to disclose personal feelings.",
