a7cc3c198f7b459d2cf89ccc972a3aeb617e241a
Prakash Pudhucode <pudhucodep@gmail.com>
2017-12-02 15:09:41 -0500

    fixed chat issue

--- modules/chat/client/config/chat.client.config.js
+++ modules/chat/client/config/chat.client.config.js
@@ -4,10 +4,10 @@
 angular.module('chat').run(['Menus',
   function (Menus) {
     // Set top bar menu items
-    Menus.addMenuItem('topbar', {
-      title: 'Chat',
-      state: 'chat'
+    //Menus.addMenuItem('topbar', {
+    //  title: 'Chat',
+    //  state: 'chat'
       //roles: ['user']
-    });
+  //  });
   }
 ]);
